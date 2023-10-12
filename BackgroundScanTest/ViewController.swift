12eb334d5bb91213524a5124fe2d75c8f705f4dc
Andrey Bashkirtcev <nruzumak@gmail.com>
2023-10-12 13:38:17 +0500

    create project

---
+++ BackgroundScanTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackgroundScanTest
+//
+//  Created by Andrey Bashkirtsev on 12.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
